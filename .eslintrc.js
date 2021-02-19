2a5cfcfd98dd6027f4b832ba92dd4126a4d4cb8a
Roger Takeshita <roger.takeshita@gmail.com>
2021-02-19 00:36:50 -0500

    feat: Should base project configuration

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    env: {
+        commonjs: true,
+        es2021: true,
+        node: true,
+    },
+    extends: ['airbnb-base'],
+    parserOptions: {
+        ecmaVersion: 12,
+    },
+    rules: {
+        'indent': ['error', 4, { "SwitchCase": 1 }],
+        'no-console': 'off',
+        'operator-linebreak': 'off',
+        'no-continue': 'off',
+        'implicit-arrow-linebreak': 'off',
+        'function-paren-newline': 'off'
+    },
+};
